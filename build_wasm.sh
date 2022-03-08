f8a5a85e53f451abd61f6be3000bd3cdea4e8fb6
Seph Gentle <me@josephg.com>
2022-03-08 15:50:46 +1100

    Bump diamond-wasm to 0.1.1

--- build_wasm.sh
+++ build_wasm.sh
@@ -11,7 +11,8 @@ echo "=== After ==="
 #wasm-pack build --target web --dev
 wasm-pack build --target web
 # sed -i '3i\ \ "type": "module",' pkg/package.json
-sed -i.old 's/"module":/"main":/' pkg/package.json 
+sed -i.old 's/"module":/"main":/' pkg/package.json
+sed -i.old 's/: "0.1.0"/: "0.1.1"/' pkg/package.json
 perl -wlpi -e 'print "  \"type\": \"module\"," if $. == 2' pkg/package.json
 
 rm pkg/package.json.old
