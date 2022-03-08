715313d7ccba2324588a872d6bb92e011ab0c015
Seph Gentle <me@josephg.com>
2022-03-08 14:42:58 +1100

    Fixed build_wasm on linux (ugh)

--- build_wasm.sh
+++ build_wasm.sh
@@ -11,8 +11,10 @@ echo "=== After ==="
 #wasm-pack build --target web --dev
 wasm-pack build --target web
 # sed -i '3i\ \ "type": "module",' pkg/package.json
-sed -i '' 's/"module":/"main":/' pkg/package.json 
+sed -i.old 's/"module":/"main":/' pkg/package.json 
 perl -wlpi -e 'print "  \"type\": \"module\"," if $. == 2' pkg/package.json
+
+rm pkg/package.json.old
 
 
 brotli -f pkg/*.wasm
