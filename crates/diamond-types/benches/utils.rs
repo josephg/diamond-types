3c8ff9ff9d73aeb4174feda3918c336abe04fea9
Seph Gentle <me@josephg.com>
2022-03-23 16:47:00 +1100

    Fixed benchmarks

--- crates/diamond-types/benches/utils.rs
+++ crates/diamond-types/benches/utils.rs
@@ -31,7 +31,7 @@ pub fn apply_edits_local(doc: &mut ListCRDT, txns: &Vec<TestTxn>) {
 pub fn apply_edits_push_merge(doc: &mut ListCRDT, txns: &Vec<TestTxn>) {
     let id = doc.get_or_create_agent_id("jeremy");
 
-    let mut last_parent = doc.branch.version[0];
+    let mut last_parent = doc.branch.local_version()[0];
 
     for (_i, txn) in txns.iter().enumerate() {
         for TestPatch(pos, del_span, ins_content) in &txn.patches {
