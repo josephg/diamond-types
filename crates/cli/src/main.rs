843b6915582c454103294dc6e3a0a9ec20f80612
Seph Gentle <me@josephg.com>
2022-03-28 01:07:47 +1100

    Added a few more options, letting you override agent and set values from some arbitrary base version

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -6,7 +6,7 @@ use rand::distributions::Alphanumeric;
 use rand::Rng;
 use similar::{ChangeTag, TextDiff};
 use similar::utils::TextDiffRemapper;
-use diamond_types::list::{ListCRDT, OpLog};
+use diamond_types::list::{Branch, OpLog};
 use diamond_types::list::encoding::{ENCODE_FULL, EncodeOptions};
 use diamond_types::list::remote_ids::RemoteId;
 
@@ -29,6 +29,12 @@ enum Commands {
         /// Equivalent to calling create followed by set.
         #[clap(short)]
         content_file: Option<String>,
+
+        /// Agent name for edits. If not specified, a random name is chosen.
+        ///
+        /// This is only relevant when content is provided. Empty files need no agent ID.
+        #[clap(short, long)]
+        agent: Option<String>,
     },
 
     /// Dump (cat) the contents of a diamond-types file to stdout or to a file
@@ -42,6 +48,9 @@ enum Commands {
         output: Option<String>,
 
         /// Checkout at the specified (requested) version
+        ///
+        /// If not specified, the version defaults to the latest version, printing the result of
+        /// merging all changes.
         #[clap(short, long, parse(try_from_str = serde_json::from_str))]
         version: Option<Box<[RemoteId]>>,
     },
@@ -81,6 +90,23 @@ enum Commands {
         /// The file containing the new content
         #[clap(parse(from_os_str))]
         target_content_file: OsString,
+
+        /// Set the new content with this version as the named parent.
+        ///
+        /// If not specified, the version defaults to the latest version (including all changes)
+        #[clap(short, long, parse(try_from_str = serde_json::from_str))]
+        version: Option<Box<[RemoteId]>>,
+
+        /// Suppress output to stdout
+        #[clap(short, long)]
+        quiet: bool,
+
+        /// Agent name for edits. If not specified, a random name is chosen.
+        ///
+        /// Be very careful overriding the default random agent name. If an (agent, seq) is ever
+        /// reused to describe two *different* edits, weird & bad things happen.
+        #[clap(short, long)]
+        agent: Option<String>,
     }
 }
 
@@ -90,15 +116,27 @@ fn parse_dt_oplog(filename: &str) -> Result<OpLog, anyhow::Error> {
     Ok(oplog)
 }
 
+// fn checkout_version_or_tip(oplog: OpLog, version: Option<&[RemoteId]>) -> Branch {
+fn checkout_version_or_tip(oplog: &OpLog, version: Option<Box<[RemoteId]>>) -> Branch {
+    let v = if let Some(version) = version {
+        oplog.try_remote_to_local_version(version.iter()).unwrap()
+    } else {
+        oplog.local_version().into() // Gross copy.
+    };
+
+    oplog.checkout(&v)
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let cli: Cli = Cli::parse();
     match cli.command {
-        Commands::Create { filename, content_file } => {
+        Commands::Create { filename, content_file, agent } => {
             let mut oplog = OpLog::new();
 
             if let Some(content_file) = content_file {
                 let content = fs::read_to_string(content_file)?;
-                let agent = oplog.get_or_create_agent_id(&random_agent_name());
+                let agent_name = agent.unwrap_or_else(|| random_agent_name());
+                let agent = oplog.get_or_create_agent_id(&agent_name);
                 oplog.add_insert(agent, 0, &content);
             }
 
@@ -112,13 +150,8 @@ fn main() -> Result<(), anyhow::Error> {
             // Using custom oplog / branch here to support custom versions
             // let oplog = OpLog::load_from(&data).unwrap();
 
-            let v = if let Some(version) = version {
-                oplog.try_remote_to_local_version(version.iter()).unwrap()
-            } else {
-                oplog.local_version().into() // Gross copy.
-            };
-
-            let branch = oplog.checkout(&v);
+            // let branch = checkout_version_or_tip(oplog, version.map(|v| &v));
+            let branch = checkout_version_or_tip(&oplog, version);
             let content = branch.content();
 
             // There's probably some fancy way to switch and share code here - either write to a
@@ -164,17 +197,31 @@ fn main() -> Result<(), anyhow::Error> {
             println!("{version}");
         }
 
-        Commands::Set { dt_filename, target_content_file } => {
+        Commands::Set { dt_filename, target_content_file, version, quiet, agent } => {
             let data = fs::read(&dt_filename)?;
             let new = fs::read_to_string(target_content_file)?;
 
-            let mut doc = ListCRDT::load_from(&data).unwrap();
-            let old = doc.branch.content().to_string();
+            let mut oplog = OpLog::load_from(&data)?;
+
+            if !quiet {
+                let v_json = if let Some(v) = version.as_ref() {
+                    // println!("Editing from requested version {}",
+                    serde_json::to_string(v)
+                } else {
+                    // println!("Editing from tip version {:?}", oplog.remote_version());
+                    serde_json::to_string(&oplog.remote_version())
+                }.unwrap();
+                println!("Editing from version {v_json}");
+            }
+
+            let mut branch = checkout_version_or_tip(&oplog, version);
+
+            let old = branch.content().to_string();
             let diff = TextDiff::from_chars(&old, &new);
             let remapper = TextDiffRemapper::from_text_diff(&diff, &old, &new);
 
-            let agent_name: String = random_agent_name();
-            let agent_id = doc.get_or_create_agent_id(&agent_name);
+            let agent_name = agent.unwrap_or_else(|| random_agent_name());
+            let agent_id = oplog.get_or_create_agent_id(&agent_name);
 
             let mut pos = 0;
             for (tag, str) in diff.ops().iter()
@@ -184,17 +231,26 @@ fn main() -> Result<(), anyhow::Error> {
                 match tag {
                     ChangeTag::Equal => pos += len,
                     ChangeTag::Delete => {
-                        doc.delete(agent_id, pos .. pos+len);
+                        // dbg!(("delete", pos .. pos+len));
+                        branch.delete(&mut oplog, agent_id, pos .. pos+len);
                     }
                     ChangeTag::Insert => {
-                        doc.insert(agent_id, pos, str);
+                        // dbg!(("insert", pos, str));
+                        branch.insert(&mut oplog, agent_id, pos, str);
                         pos += len;
                     }
                 }
             }
 
+            if !quiet {
+                println!("Resulting branch version after changes {}",
+                         serde_json::to_string(&branch.remote_version(&oplog)).unwrap());
+                println!("Resulting file version after changes {}",
+                         serde_json::to_string(&oplog.remote_version()).unwrap());
+            }
+
             // TODO: Do that atomic rename nonsense instead of just overwriting.
-            let out_data = doc.oplog.encode(EncodeOptions::default());
+            let out_data = oplog.encode(EncodeOptions::default());
             fs::write(&dt_filename, out_data)?;
         }
     }
