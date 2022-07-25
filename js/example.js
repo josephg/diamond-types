6d0e22338d57397244c997e6141cc9e388963cfa
Seph Gentle <me@josephg.com>
2022-07-25 13:22:18 +1000

    Added support for sets to the JS code

--- js/example.js
+++ js/example.js
@@ -1,41 +1,38 @@
+const assert = require('assert/strict')
 const dt = require('./dist')
 
 const db = dt.createDb()
 
-dt.applyLocalOp(db, ['seph', 0], dt.ROOT, {
-  type: 'map',
-  key: 'yo',
-  val: {type: 'primitive', val: 123}
-})
+dt.localMapInsert(db, ['seph', 0], dt.ROOT, 'yo', {type: 'primitive', val: 123})
+assert.deepEqual(db.value, {yo: 123})
 
+// concurrent changes
 dt.applyRemoteOp(db, {
   id: ['mike', 0],
   globalParents: [],
-  localParents: [],
   crdtId: dt.ROOT,
-  action: {type: 'map', key: 'yo', val: {type: 'primitive', val: 321}},
+  action: {type: 'map', localParents: [], key: 'yo', val: {type: 'primitive', val: 321}},
 })
+assert.deepEqual(db.value, {yo: 123})
 
 dt.applyRemoteOp(db, {
   id: ['mike', 1],
   globalParents: [['mike', 0], ['seph', 0]],
-  localParents: [['mike', 0], ['seph', 0]],
   crdtId: dt.ROOT,
-  action: {type: 'map', key: 'yo', val: {type: 'primitive', val: 1000}},
+  action: {type: 'map', localParents: [['mike', 0], ['seph', 0]], key: 'yo', val: {type: 'primitive', val: 1000}},
 })
+assert.deepEqual(db.value, {yo: 1000})
+
+// Set a value in an inner map
+const inner = dt.localMapInsert(db, ['seph', 1], dt.ROOT, 'stuff', {type: 'crdt', crdtKind: 'map'})
+dt.localMapInsert(db, ['seph', 2], inner.id, 'cool', {type: 'primitive', val: 'definitely'})
+assert.deepEqual(db.value, {yo: 1000, stuff: {cool: 'definitely'}})
 
 
-const inner = dt.applyLocalOp(db, ['seph', 1], dt.ROOT, {
-  type: 'map',
-  key: 'stuff',
-  val: {type: 'crdt', crdtKind: 'map'}
-})
-dt.applyLocalOp(db, ['seph', 2], inner.id, {
-  type: 'map',
-  key: 'cool',
-  val: {type: 'primitive', val: 'definitely'}
-})
-
+// Insert a set
+const innerSet = dt.localMapInsert(db, ['seph', 2], dt.ROOT, 'a set', {type: 'crdt', crdtKind: 'set'})
+dt.localSetInsert(db, ['seph', 3], innerSet.id, {type: 'primitive', val: 'whoa'})
+dt.localSetInsert(db, ['seph', 4], innerSet.id, {type: 'crdt', crdtKind: 'map'})
 
 console.log('db', db.value)
 console.log('db', db)
--- js/src/index.ts
+++ js/src/index.ts
@@ -1,6 +1,4 @@
 // This is a simplified database for the browser. No history is stored.
-
-import assert from "assert/strict"
 import Map2 from "map2"
 
 export const ROOT: Version = ['ROOT', 0]
@@ -10,10 +8,10 @@ export type Version = [agent: string, seq: number]
 type Primitive = null | boolean | string | number | Primitive[] | {[k: string]: Primitive}
 
 type CreateValue = {type: 'primitive', val: Primitive}
-  | {type: 'crdt', crdtKind: 'map'}
+  | {type: 'crdt', crdtKind: 'map' | 'set'}
 
 type RegisterValue = {type: 'primitive', val: Primitive}
-  | {type: 'crdt', crdtKind: 'map', id: Version}
+  | {type: 'crdt', id: Version}
 
 type MVRegister = [Version, RegisterValue][]
 
@@ -21,6 +19,10 @@ type CRDTInfo = {
   type: 'map',
   registers: {[k: string]: MVRegister},
   activeValue: {[k: string]: any},
+} | {
+  type: 'set',
+  values: Map2<string, number, RegisterValue>,
+  activeValue: Map2<string, number, any>,
 }
 
 interface DBState {
@@ -29,14 +31,14 @@ interface DBState {
   crdts: Map2<string, number, CRDTInfo>
 }
 
-type Action = {
-  type: 'map', key: string, val: CreateValue
-}
+type Action =
+{ type: 'map', key: string, localParents: Version[], val: CreateValue }
+| { type: 'setInsert', val: CreateValue }
+| { type: 'setDelete', target: Version }
 
 interface Operation {
   id: Version,
   globalParents: Version[],
-  localParents: Version[],
   crdtId: Version,
   action: Action,
 }
@@ -70,36 +72,99 @@ export function createDb(): DBState {
   return db
 }
 
-function removeRecursive(state: DBState, crdtId: Version) {
-  const crdt = state.crdts.get(crdtId[0], crdtId[1])
-  if (crdt != null) {
-    for (const k in crdt.registers) {
-      const reg = crdt.registers[k]
-      for (const [version, value] of reg) {
-        if (value.type === 'crdt') {
-          removeRecursive(state, value.id)
+function removeRecursive(state: DBState, value: RegisterValue) {
+  if (value.type !== 'crdt') return
+
+  const crdt = state.crdts.get(value.id[0], value.id[1])
+  if (crdt == null) return
+
+  switch (crdt.type) {
+    case 'map':
+      for (const k in crdt.registers) {
+        const reg = crdt.registers[k]
+        for (const [version, value] of reg) {
+          removeRecursive(state, value)
         }
       }
-    }
-
-    state.crdts.delete(crdtId[0], crdtId[1])
+      break
+    case 'set':
+      for (const [agent, seq, value] of crdt.values) {
+        removeRecursive(state, value)
+      }
+      break
+    default: throw Error('Unknown CRDT type!?')
   }
+
+  state.crdts.delete(value.id[0], value.id[1])
 }
 
-export function applyLocalOp(state: DBState, id: Version, crdtId: Version, action: Action): Operation {
-  const crdt = state.crdts.get(crdtId[0], crdtId[1])
-  if (crdt == null) throw Error('invalid CRDT')
+export function localMapInsert(state: DBState, id: Version, mapId: Version, key: string, val: CreateValue): Operation {
+  const crdt = state.crdts.get(mapId[0], mapId[1])
+  if (crdt == null || crdt.type !== 'map') throw Error('invalid CRDT')
 
-  const localParents = (crdt.registers[action.key] ?? []).map(([version]) => version)
+  const localParents = (crdt.registers[key] ?? []).map(([version]) => version)
   const op: Operation = {
     id,
+    crdtId: mapId,
     globalParents: state.version,
-    localParents,
-    crdtId,
-    action
+    action: { type: 'map', localParents, key, val }
   }
+  // TODO: Could easily inline this - which would mean more code but higher performance.
   applyRemoteOp(state, op)
   return op
+}
+
+export function localSetInsert(state: DBState, id: Version, setId: Version, val: CreateValue): Operation {
+  const crdt = state.crdts.get(setId[0], setId[1])
+  if (crdt == null || crdt.type !== 'set') throw Error('invalid CRDT')
+
+  const op: Operation = {
+    id,
+    crdtId: setId,
+    globalParents: state.version,
+    action: { type: 'setInsert', val }
+  }
+  // TODO: Inline this?
+  applyRemoteOp(state, op)
+  return op
+}
+
+export function localSetDelete(state: DBState, id: Version, setId: Version, target: Version): Operation | null {
+  const crdt = state.crdts.get(setId[0], setId[1])
+  if (crdt == null || crdt.type !== 'set') throw Error('invalid CRDT')
+
+  let oldVal = crdt.values.get(target[0], target[1])
+  if (oldVal != null) {
+    removeRecursive(state, oldVal)
+    crdt.values.delete(target[0], target[1])
+
+    return {
+      id,
+      crdtId: setId,
+      globalParents: state.version,
+      action: { type: 'setDelete', target }
+    }
+  } else { return null } // Already deleted.
+}
+
+
+function createCRDT(state: DBState, id: Version, type: 'map' | 'set'): any {
+  if (state.crdts.has(id[0], id[1])) {
+    throw Error('CRDT already exists !?')
+  }
+
+  const crdtInfo: CRDTInfo = type === 'map' ? {
+    type: "map",
+    registers: {},
+    activeValue: {}
+  } : {
+    type: 'set',
+    values: new Map2,
+    activeValue: new Map2,
+  }
+
+  state.crdts.set(id[0], id[1], crdtInfo)
+  return crdtInfo.activeValue
 }
 
 export function applyRemoteOp(state: DBState, op: Operation) {
@@ -112,50 +177,74 @@ export function applyRemoteOp(state: DBState, op: Operation) {
   }
 
   // Every map operation creates a new value, and removes 0-n other values.
-  assert.equal(op.action.type, 'map')
+  switch (op.action.type) {
+    case 'map':
+      if (crdt.type !== 'map') throw Error('Invalid operation type for target')
 
-  const oldPairs = crdt.registers[op.action.key] ?? []
-  const newPairs: MVRegister = []
-  for (const [version, value] of oldPairs) {
-    // Each item is either retained or removed.
-    if (op.localParents.some(v2 => versionEq(version, v2))) {
-      // The item was named in parents. Remove it.
-      console.log('removing', value)
-      if (value.type === 'crdt') {
-        removeRecursive(state, value.id)
+      const oldPairs = crdt.registers[op.action.key] ?? []
+      const newPairs: MVRegister = []
+      for (const [version, value] of oldPairs) {
+        // Each item is either retained or removed.
+        if (op.action.localParents.some(v2 => versionEq(version, v2))) {
+          // The item was named in parents. Remove it.
+          console.log('removing', value)
+          removeRecursive(state, value)
+        } else {
+          newPairs.push([version, value])
+        }
       }
-    } else {
-      newPairs.push([version, value])
-    }
+    
+      let newValue: RegisterValue
+      if (op.action.val.type === 'primitive') {
+        newValue = op.action.val
+      } else {
+        // Create it.
+        createCRDT(state, op.id, op.action.val.crdtKind)
+        newValue = {type: "crdt", id: op.id}
+      }
+    
+      newPairs.push([op.id, newValue])
+      newPairs.sort(([v1], [v2]) => versionCmp(v1, v2))
+    
+      // When there's a tie, the active value is based on the order in pairs.
+      const activeValue = newPairs[0][1]
+      if (activeValue.type === 'primitive') {
+        crdt.activeValue[op.action.key] = activeValue.val
+      } else {
+        crdt.activeValue[op.action.key] = state.crdts.get(activeValue.id[0], activeValue.id[1])!.activeValue
+      }
+    
+      crdt.registers[op.action.key] = newPairs
+      break
+
+    case 'setInsert': case 'setDelete': // Sets!
+      if (crdt.type !== 'set') throw Error('Invalid operation type for target')
+
+      // Set operations are comparatively much simpler, because insert
+      // operations cannot be concurrent and multiple overlapping delete
+      // operations are ignored.
+
+      if (op.action.type == 'setInsert') {
+        if (op.action.val.type === 'primitive') {
+          crdt.values.set(op.id[0], op.id[1], op.action.val)
+          crdt.activeValue.set(op.id[0], op.id[1], op.action.val.val)
+        } else {
+          const activeValue = createCRDT(state, op.id, op.action.val.crdtKind)
+          crdt.values.set(op.id[0], op.id[1], {type: "crdt", id: op.id})
+          crdt.activeValue.set(op.id[0], op.id[1], activeValue)
+        }
+      } else {
+        // Delete!
+        let oldVal = crdt.values.get(op.action.target[0], op.action.target[1])
+        if (oldVal != null) {
+          removeRecursive(state, oldVal)
+          crdt.values.delete(op.action.target[0], op.action.target[1])
+        }
+      }
+
+      break
+
+    default: throw Error('Invalid action type')
   }
 
-  let newValue: RegisterValue
-  if (op.action.val.type === 'primitive') {
-    newValue = op.action.val
-  } else {
-    // Create it.
-    if (state.crdts.has(op.id[0], op.id[1])) {
-      throw Error('CRDT already exists !?')
-    }
-
-    newValue = {type: "crdt", crdtKind: "map", id: op.id}
-    state.crdts.set(op.id[0], op.id[1], {
-      type: "map",
-      registers: {},
-      activeValue: {}
-    })
-  }
-
-  newPairs.push([op.id, newValue])
-  newPairs.sort(([v1], [v2]) => versionCmp(v1, v2))
-
-  // When there's a tie, the active value is based on the order in pairs.
-  const activeValue = newPairs[0][1]
-  if (activeValue.type === 'primitive') {
-    crdt.activeValue[op.action.key] = activeValue.val
-  } else {
-    crdt.activeValue[op.action.key] = state.crdts.get(activeValue.id[0], activeValue.id[1])!.activeValue
-  }
-
-  crdt.registers[op.action.key] = newPairs
 }
