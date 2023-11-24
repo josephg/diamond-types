d163b90bb4905b3c5ef1530f3797351c0798f9b3
Seph Gentle <me@josephg.com>
2023-11-24 17:37:33 +1100

    Added clownschool to set of targets. Benchmark still using old algorithm.

--- crates/bench/src/main.rs
+++ crates/bench/src/main.rs
@@ -15,7 +15,7 @@ fn testing_data(name: &str) -> TestData {
 }
 
 const LINEAR_DATASETS: &[&str] = &["automerge-paper", "rustcode", "sveltecomponent", "seph-blog1", "friendsforever_flat"];
-const COMPLEX_DATASETS: &[&str] = &["node_nodecc", "git-makefile", "friendsforever"];
+const COMPLEX_DATASETS: &[&str] = &["node_nodecc", "git-makefile", "friendsforever", "clownschool"];
 
 fn local_benchmarks(c: &mut Criterion) {
     for name in LINEAR_DATASETS {
