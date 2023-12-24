79109bbfc0282f09986eb2b85cdcbb50d11d0580
Seph Gentle <me@josephg.com>
2023-12-24 20:51:51 +1100

    Updated benchmark script

--- bench.sh
+++ bench.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 set -e
-set -o xtrace
+#set -o xtrace
+
+start_time=$(date +%s)   # Capture start time in seconds
 
 cargo build --release -p bench
-sleep 5
-taskset 0x1 nice -10 cargo run --release -p bench -- --bench $@
+
+end_time=$(date +%s)     # Capture end time in seconds
+# Calculate duration
+duration=$((end_time - start_time))
+
+# Check if duration is less than 1 second
+if [ $duration -gt 1 ]; then
+  echo "Waiting 5s for CPU to cool down"
+  sleep 5
+fi
+
+#taskset 0x1 nice -10 cargo run --release -p bench -- --bench $@
+taskset 0x1 nice -10 target/release/bench --bench "$@"
